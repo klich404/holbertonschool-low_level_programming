e7b8b7b206d8cec5a4b970f79545ee77549fa580
klich404 <klich4010@gmail.com>
2021-02-03 17:13:56 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - write a string
+ * Return: 0
  */
 
-void main ()
+void main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
