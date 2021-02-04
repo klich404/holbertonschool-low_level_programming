93624c17d1ba6b2e565de28e1975a33324046666
klich404 <klich4010@gmail.com>
2021-02-04 17:45:43 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printf the numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar (x + 0);
+	}
+
+	putchar (10);
+	return (0);
+}
