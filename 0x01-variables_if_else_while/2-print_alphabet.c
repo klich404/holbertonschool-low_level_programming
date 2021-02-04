a0b8565814889fdcc55ab0ef4baa611d2e2d06d5
klich404 <klich4010@gmail.com>
2021-02-04 17:06:30 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printf the alphabeth
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char x;
+
+	for (x = 97; x < 123; x++)
+	{
+		putchar (x + 0);
+	}
+	putchar (10);
+	return (0);
+}
