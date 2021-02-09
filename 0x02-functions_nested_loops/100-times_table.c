1e0cbccfa10a6d9c01acb37e6eb2f35ef5f6793a
klich404 <klich4010@gmail.com>
2021-02-09 20:56:25 +0000

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print the n table
+ * @n: an int variable
+ * Return: nothing
+ */
+
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n > 15 || n < 0)
+		return;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+
+			if (z > 99)
+			{
+				_putchar(z / 100 + 48);
+				_putchar((z / 10 % 10) + 48);
+				_putchar(z % 10 + 48);
+			}
+
+			else if (z > 9)
+			{
+				_putchar(32);
+				_putchar(z / 10 + 48);
+				_putchar(z % 10 + 48);
+			}
+			else if (y != 0)
+			{
+				_putchar(32);
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar(z + 48);
+			}
+			if (y != n)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+		_putchar(10);
+	}
+}
