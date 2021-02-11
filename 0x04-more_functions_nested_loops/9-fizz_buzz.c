c6617ffaf6da1d33040bc23548289aedb839e8b6
klich404 <klich4010@gmail.com>
2021-02-11 18:55:43 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if ((x % 3) == 0 && (x % 5) == 0)
 			printf("FizzBuzz");
 
-		else if (x % 3 == 0)
+		else if ((x % 3) == 0)
 			printf("Fizz");
 
-		else if (x % 5 == 0)
+		else if ((x % 5) == 0)
 			printf("Buzz");
 
 		else
@@ -25,6 +25,7 @@ int main(void)
 
 		putchar (32);
 	}
+
 	putchar (10);
 	return (0);
 }
