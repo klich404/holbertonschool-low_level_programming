45bb8a17b0d676bbe6fb6112c262c2d1abdd9937
klich404 <klich4010@gmail.com>
2021-02-11 19:23:38 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for(y = size - x; y > 0; y--)
+				_putchar(32);
+			
+			for(y = 0; y < x; y++)
+				_putchar(35);
+			
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
