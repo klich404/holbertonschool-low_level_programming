e2672bc760e041a8210c680f25a7c307d3f4c00e
klich404 <klich4010@gmail.com>
2021-02-16 21:04:27 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,21 @@ void puts_half(char *str)
 	for (x = 0; str[x] != 00; x++)
 		;
 
-	for (x /= 2; str[x] != 00; x++)
+	if (x % 2 == 0)
 	{
-		_putchar(str[x]);
+		for (x /= 2; str[x] != 00; x++)
+		{
+			_putchar(str[x]);
+		}
+		_putchar(10);
 	}
-	_putchar(10);
+	else
+	{
+		x++;
+		for (x /= 2; str[x] != 00; x++)
+		{
+			_putchar(str[x]);
+		}
+		_putchar(10);
+	}
 }
