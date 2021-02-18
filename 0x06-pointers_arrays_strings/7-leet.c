7e38fcbe7289c40c596c06bb9a63c8b8a314ee1d
klich404 <klich4010@gmail.com>
2021-02-18 19:45:26 +0000

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: is a pointer
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	char *orig, *repl;
+	int x, y;
+
+	orig = "aAeEoOtTlL";
+	repl = "4433007711";
+
+	for (x = 0;str[x] != 00; x++)
+	{
+		for (y = 0; orig[y] != 00; y++)
+		{
+			if(str[x] == orig[y])
+			{
+				str[x] = repl[y];
+			}
+		}
+	}
+	return (str);
+}
