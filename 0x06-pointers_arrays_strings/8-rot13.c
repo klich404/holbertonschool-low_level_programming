bb138ad6ec96f6e9001389d658715209d856c1f3
klich404 <klich4010@gmail.com>
2021-02-18 21:02:15 +0000

    task 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -15,7 +15,8 @@ char *rot13(char *str)
 		for (; (str[x] >= 65 && str[x] <= 90) ||
 			     (str[x] >= 97 && str[x] <= 122); x++)
 		{
-			if ((str[x] >= 65 && str[x] <= 77) || (str[x] >= 97 && str[x] <= 109))
+			if ((str[x] >= 65 && str[x] <= 77) ||
+			    (str[x] >= 97 && str[x] <= 109))
 			{
 				str[x] += 13;
 			}
