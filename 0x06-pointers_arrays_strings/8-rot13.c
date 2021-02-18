8bb61bb3e01e597111960d06c2a88e36e776bbf8
klich404 <klich4010@gmail.com>
2021-02-18 21:00:37 +0000

    task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: is a pointer
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != 00; x++)
+	{
+		for (; (str[x] >= 65 && str[x] <= 90) ||
+			     (str[x] >= 97 && str[x] <= 122); x++)
+		{
+			if ((str[x] >= 65 && str[x] <= 77) || (str[x] >= 97 && str[x] <= 109))
+			{
+				str[x] += 13;
+			}
+			else
+			{
+				str[x] -= 13;
+			}
+		}
+	}
+	return (str);
+}
