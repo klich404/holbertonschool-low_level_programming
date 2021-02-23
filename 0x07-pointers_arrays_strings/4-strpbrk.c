e3d14b862a36db92d85ee95ef44bed5275d03fcf
klich404 <klich4010@gmail.com>
2021-02-23 18:36:02 +0000

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: is a pointer
+ * @accept: is a pointer
+ * Return: s + x
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; s[x] != 00; x++)
+	{
+		for (y = 0; accept[y] != 00; y++)
+		{
+			if(s[x] == accept[y])
+			{
+				return (s + x);
+			}
+		}
+	}
+	return (0);
+}
