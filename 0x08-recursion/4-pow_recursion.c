2ed3df5bbbe35bb2275e5cf8b8b4f047f89ef318
klich404 <klich4010@gmail.com>
2021-02-24 22:49:37 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
 
 /**
- *
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: is an integer
+ * @y: is an integer
+ * Return: x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
