f615beff9fc2aff6cb179d5df6f31e4febf5bea6
klich404 <klich4010@gmail.com>
2021-02-24 22:03:58 +0000

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: is a pointer
+ * Return: length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == 00)
+		return(0);
+
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
