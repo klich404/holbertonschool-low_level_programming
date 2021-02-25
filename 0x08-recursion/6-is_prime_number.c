90b4bcb63e265ac7a1b0685a8484788b2a05e780
klich404 <klich4010@gmail.com>
2021-02-25 21:47:23 +0000

    task 6 and header

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns 1 n is a prime number, otherwise return 0
+ * @n: is an integer
+ * Return: the prime number of n
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	else
+		return (cal(n, n - 1));
+}
+
+/**
+ * cal - calculate the prime number of n
+ * @x: is an integer
+ * @y: is an integer
+ * Return: a prime number
+ */
+
+int cal(int x, int y)
+{
+	if (y == 1)
+		return (1);
+
+	else
+	{
+		if(x % y == 0)
+			return (0);
+
+		else
+			return(cal(x, y - 1));
+	}
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,5 +7,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int revision(int x, int y);
 int is_prime_number(int n);
+int cal(int x, int y);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
