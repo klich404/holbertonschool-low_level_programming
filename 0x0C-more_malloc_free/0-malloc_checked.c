03f4ac0938271056d60e36e0d89c10eba88bd6fd
klich404 <klich4010@gmail.com>
2021-03-04 19:23:41 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: is an unsigned int
+ * Return: ptr
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == 0)
+		exit (98);
+
+	return (ptr);
+}
