0e1d7a45863ee723e4e538945c3dd2e98114e8ee
klich404 <klich4010@gmail.com>
2021-03-08 17:28:12 +0000

    task 3 and 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
 #define ABS(X) ((X) < 0 ? (X) * -1 : (X))
 #endif
