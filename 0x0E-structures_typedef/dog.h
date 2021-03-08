69a431c4e6c673d18e5bdba32472498a8fc9be12
klich404 <klich4010@gmail.com>
2021-03-08 19:50:47 +0000

    README.md dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a data structure
+ * @name: is a character
+ * @age: is a float
+ * @owner: is a char
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
