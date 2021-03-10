1cb31cfe60a431d561688c33b09c49471271e20d
klich404 <klich4010@gmail.com>
2021-03-10 23:44:56 +0000

    task 4

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (action == 0)
+	if (action == 0 || array == 0)
 		return;
 
 	for (x = 0; x < size; x++)
-	{
 		(*action)(array[x]);
-	}
 }
