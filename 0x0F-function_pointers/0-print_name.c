f3f1db2bf4bbca20736eb53850e2dcc071b2be4a
klich404 <klich4010@gmail.com>
2021-03-10 18:54:40 +0000

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,9 @@
 #include "function_pointers.h"
 
 /**
- *
+ * print_name - prints a name
+ * @name: is a pointer character
+ * @f: is a function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
