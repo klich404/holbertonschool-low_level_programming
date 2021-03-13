379e027de11170555f32f81cbb8d222f1f64ed71
klich404 <klich4010@gmail.com>
2021-03-13 21:27:50 +0000

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	int x = 0, y = 0;
+	char *sep = "";
+
+	fm_t sea_for[] = {
+		{"c", character},
+		{"i", integer},
+		{"f", floatt},
+		{"s", string},
+		{NULL, NULL}
+	};
+
+	va_start(arg, format);
+
+	while (format[x] != 00)
+	{
+		while (sea_for[y].ident != 0)
+		{
+			if (sea_for[y].ident[0] == format[x])
+			{
+				printf("%s", sep);
+				sea_for[y].f(arg);
+				sep = ", ";
+			}
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	va_end(arg);
+	printf("\n");
+}
+
+/**
+ * character - print a character
+ * @print: the caracter
+ */
+void character(va_list print)
+{
+	printf("%c", va_arg(print, int));
+}
+
+/**
+ * integer - print an integer
+ * @print: the integer
+ */
+void integer(va_list print)
+{
+	printf("%d", va_arg(print, int));
+}
+
+/**
+ * floatt - print a float
+ * @print: the float
+ */
+void floatt(va_list print)
+{
+	printf("%f", va_arg(print, double));
+}
+
+/**
+ * string - print a string
+ * @print: the string
+ */
+void string(va_list print)
+{
+	if (print == 0)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", va_arg(print, char*));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void character(va_list print);
+void integer(va_list print);
+void floatt(va_list print);
+void string(va_list print);
 
+/**
+ * struct form - search the format
+ * @ident: the identifier
+ * @f: the function
+ */
+typedef struct form
+{
+	char *ident;
+	void (*f)(va_list);
+} fm_t;
 
 #endif
