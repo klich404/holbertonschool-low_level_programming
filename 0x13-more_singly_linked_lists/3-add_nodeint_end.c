4de99b22e94de17f075885dc009f27b8d9b47e6e
klich404 <klich4010@gmail.com>
2021-03-23 16:14:13 +0000

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * ass_nodeint_end - adds a new node at the end of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: is the head of the list
  * @n: is an integer
  * Return: *head
