dc99b4ab68aa26e462de6b06522dc16889756629
klich404 <klich4010@gmail.com>
2021-03-24 17:47:59 -0500

    lists.h

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,3 +1,26 @@
-Visual Studio
-prueba
-Ubuntu
+#include "lists.h"
+
+/**
+ * get_nodeint_a - returns the nth node of a listint_t linked list.
+ * @head: is the head of the list
+ * @index: is the node of the list who whe want to return
+ * Return: head
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x;
+
+	for (x = 0; x <= index; x++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		if (x == index)
+			return (head);
+
+		head = head->next;
+	}
+
+	return (head);
+}
