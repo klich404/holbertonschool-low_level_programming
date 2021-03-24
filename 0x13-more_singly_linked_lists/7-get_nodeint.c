f534041076f341853f723203e79d095148cdd213
klich404 <klich4010@gmail.com>
2021-03-24 17:48:24 -0500

    lists.h

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_a - returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @head: is the head of the list
  * @index: is the node of the list who whe want to return
  * Return: head
