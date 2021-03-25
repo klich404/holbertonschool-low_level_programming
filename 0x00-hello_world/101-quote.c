9399915d1d02421300d6233776dcbe8db1747c32
klich404 <klich4010@gmail.com>
2021-03-24 19:10:47 -0500

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - prints a quote
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		  58);
+	return (1);
+}
