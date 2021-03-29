aa37533791f1c8bfb101638e713efd5b0ea8b7b8
klich404 <klich4010@gmail.com>
2021-03-29 18:46:04 -0500

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, count, wr;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
