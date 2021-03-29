44facd1523aa55e4044f140fb126ef1d7c98a1db
klich404 <klich4010@gmail.com>
2021-03-29 18:46:56 -0500

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, count, wr;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
