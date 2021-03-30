fd5ea6bb5b4487c53919735c768d34d4d9f1f95f
klich404 <klich4010@gmail.com>
2021-03-29 21:36:20 -0500

    header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,5 +8,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error98(char *file);
+void error99(char *file);
+void error100(char *file);
 
 #endif
