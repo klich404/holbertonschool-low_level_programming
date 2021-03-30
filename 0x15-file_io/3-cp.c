c986ddad7994d2a99bf8cf7a5ae073fc5316cbc9
klich404 <klich4010@gmail.com>
2021-03-29 21:05:09 -0500

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int fd1, fd2, rd, wr;
+	char *file_from, *file_to, buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	rd = read(fd1, buffer, 1024);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	wr = write(fd2, buffer, rd);
+	if (wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	close(fd1);
+	close(fd2);
+	return (0);
+}
