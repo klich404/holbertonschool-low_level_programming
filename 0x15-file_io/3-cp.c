bf3b1fefadf6a20cb502640261de2d4612e6f5ee
klich404 <klich4010@gmail.com>
2021-03-29 21:34:03 -0500

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,45 +23,57 @@ int main(int argc, char **argv)
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+	error98(file_from);
 
 	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		error99(file_to);
 
 	rd = read(fd1, buffer, 1024);
 	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		error98(file_from);
 
 	wr = write(fd2, buffer, rd);
 	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		error99(file_to);
 
 	cs1 = close(fd1);
 	if (cs1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		error100(fd1);
 
 	cs2 = close(fd2);
 	if (cs2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
+		error100(fd2);
 
 	return (0);
+}
+
+/**
+ * error98 - if file_from does not exist, or if you can not read it
+ * @file: the first argument passed to your program
+ */
+void error98(*file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+/**
+ * error99 - if you can not create or if write to file_to fails
+ *@file: the second argument passed to your program
+ */
+void error99(*file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+/**
+ * error100 - if you can not close a file descriptor
+ * @file: the value of the file descriptor
+ */
+void error100(*file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+	exit(100);
 }
