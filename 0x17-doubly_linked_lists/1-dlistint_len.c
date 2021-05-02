8ac4a80b1be2e94260b935ce96fa65bda9868ac1
klich404 <klich4010@gmail.com>
2021-05-02 15:56:48 -0500

    correction task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,21 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t x;
+	int check;
 
 	for (x = 0; h != NULL; x++)
 	{
-		if (h->n != 00)
+		if (h->prev != NULL && check == 0)
 		{
-			h = h->next;
+			h = h->prev;
+			continue;
 		}
+		else if (h->prev == NULL)
+		{
+			check = 1;
+			x = 0;
+		}
+		h = h->next;
 	}
 	return (x);
 }
